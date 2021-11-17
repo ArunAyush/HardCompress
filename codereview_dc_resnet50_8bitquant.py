c3456fe3d95e1dbf8d75ff31f01433517268015e
ArunAyush <arunachalamayush@gmail.com>
2021-11-16 20:17:36 -0600

    Add files via upload

---
+++ codereview_dc_resnet50_8bitquant.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+from tensorflow import keras
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import model_from_json
+from numpy import load
+from collections import defaultdict
+from collections import Counter
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+
+origModel = ResNet50(weights='imagenet')
+
+# Serializing model to JSON
+finalTrainedModel_json = origModel.to_json()
+with open("pretrainedModel_ResNet50_ImageNet.json", "w") as json_file:
+    json_file.write(finalTrainedModel_json)
+
+# Serializing weights to HDF5
+origModel.save_weights("pretrainedModel_ResNet50_ImageNet.h5")
+print("Model saved locally")
+
+for i in range(len(origModel.weights)):
+  print("Layer", i+1, "has shape:", np.shape(origModel.weights[i]))
+
+# weightList = [x for x in range(0, 31, 2)]
+weightList = [0, 6, 12, 18, 20, 30, 36, 42, 48, 54, 60, 66, 72, 78, 80, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144, 150, 156, 158, 168, 174, 180, 186, 192, 198, 204, 210, 216, 222, 228, 234, 240, 246, 252, 258, 264, 270, 272, 282, 288, 294, 300, 306, 312, 318]
+print(weightList)
+
+# load json and create model
+json_file = open('pretrainedModel_ResNet50_ImageNet.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+
+trainedModel = model_from_json(loaded_model_json)
+# load weights into new model
+trainedModel.load_weights("pretrainedModel_ResNet50_ImageNet.h5")
+print("Loaded model from disk")
+trainedWeights = np.array(trainedModel.get_weights())
+print (len(trainedWeights))
+
+# for i in range(len(trainedWeights)):
+#   print("Layer", i+1, "has shape:", np.shape(trainedWeights[i]))
+
+maxWeightArray = []
+for i in weightList:
+  layer = trainedWeights[i]
+  layer = layer.flatten()
+  maxWeightArray.append(abs(max(layer)))
+
+print(maxWeightArray)
+
+maxWeightGlobal = max(maxWeightArray)
+print(maxWeightGlobal)
+
+quantizedWeightArray = []
+for i in weightList:
+  layer = trainedWeights[i]
+  layer = layer.flatten()
+  layer = layer / maxWeightGlobal
+  layer = layer * 255
+  layer = layer.astype(int)
+  quantizedWeightArray.append(layer)
+
+print(len(quantizedWeightArray))
+
+quantizedWeightArray
+
+absWeightValues = quantizedWeightArray
+print(np.shape(absWeightValues))
+print(len(absWeightValues))
+# absWeightValues = []
+for i in range(len(absWeightValues)):
+	absWeightValues[i] = abs(quantizedWeightArray[i])
+
+np.shape(absWeightValues)
+
+weightCount = 0
+for i in range(len(absWeightValues)):
+  # print(np.shape(absWeightValues[i]))
+  weightCount += len(absWeightValues[i])
+
+print(weightCount)
+
+absWeightValuesConcat = np.concatenate(absWeightValues, axis=0)
+print(len(absWeightValuesConcat))
+
+uniqueVals = np.unique(absWeightValuesConcat)
+print("The unique weight values are: ")
+print(uniqueVals)
+print("The number of unique weight values is:", len(uniqueVals))
+
+dictionaryEntries = Counter(absWeightValuesConcat).most_common(len(uniqueVals))
+print(dictionaryEntries)
+
+for i in range(len(dictionaryEntries)):
+  if dictionaryEntries[i][0] == 0:
+    print("Count of 0s: ", dictionaryEntries[i][1], "Percentage of 0s: ", dictionaryEntries[i][1]/len(absWeightValuesConcat))
+  if dictionaryEntries[i][0] == 1:
+    print("Count of 1s: ", dictionaryEntries[i][1], "Percentage of 1s: ", dictionaryEntries[i][1]/len(absWeightValuesConcat))
---
+++ codereview_dc_vgg19_8bitquant.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+from tensorflow import keras
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import model_from_json
+from numpy import load
+from collections import defaultdict
+from collections import Counter
+
+print(tf.__version__)
+
+origModel = tf.keras.applications.VGG19(include_top=True, weights='imagenet')
+
+origModel.summary()
+
+# Serializing model to JSON
+finalTrainedModel_json = origModel.to_json()
+with open("pretrainedModel_VGG19_ImageNet.json", "w") as json_file:
+    json_file.write(finalTrainedModel_json)
+
+# Serializing weights to HDF5
+origModel.save_weights("pretrainedModel_VGG19_ImageNet.h5")
+print("Model saved locally")
+
+for i in range(len(origModel.weights)):
+  print("Layer", i+1, "has shape:", np.shape(origModel.weights[i]))
+
+weightList = [x for x in range(0, 37, 2)]
+print(weightList)
+
+# load json and create model
+json_file = open('pretrainedModel_VGG19_ImageNet.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+
+trainedModel = model_from_json(loaded_model_json)
+# load weights into new model
+trainedModel.load_weights("pretrainedModel_VGG19_ImageNet.h5")
+print("Loaded model from disk")
+trainedWeights = np.array(trainedModel.get_weights())
+print (len(trainedWeights))
+
+# for i in range(len(trainedWeights)):
+#   print("Layer", i+1, "has shape:", np.shape(trainedWeights[i]))
+
+maxWeightArray = []
+for i in weightList:
+  layer = trainedWeights[i]
+  layer = layer.flatten()
+  maxWeightArray.append(abs(max(layer)))
+
+print(maxWeightArray)
+
+maxWeightGlobal = max(maxWeightArray)
+print(maxWeightGlobal)
+
+quantizedWeightArray = []
+for i in weightList:
+  layer = trainedWeights[i]
+  layer = layer.flatten()
+  layer = layer / maxWeightGlobal
+  layer = layer * 255
+  layer = layer.astype(int)
+  quantizedWeightArray.append(layer)
+
+print(len(quantizedWeightArray))
+
+quantizedWeightArray
+
+absWeightValues = quantizedWeightArray
+print(np.shape(absWeightValues))
+print(len(absWeightValues))
+# absWeightValues = []
+for i in range(len(absWeightValues)):
+	absWeightValues[i] = abs(quantizedWeightArray[i])
+
+np.shape(absWeightValues)
+
+weightCount = 0
+for i in range(len(absWeightValues)):
+  # print(np.shape(absWeightValues[i]))
+  weightCount += len(absWeightValues[i])
+
+print(weightCount)
+
+absWeightValuesConcat = np.concatenate(absWeightValues, axis=0)
+print(len(absWeightValuesConcat))
+
+uniqueVals = np.unique(absWeightValuesConcat)
+print("The unique weight values are: ")
+print(uniqueVals)
+print("The number of unique weight values is:", len(uniqueVals))
+
+dictionaryEntries = Counter(absWeightValuesConcat).most_common(len(uniqueVals))
+print(dictionaryEntries)
+
+for i in range(len(dictionaryEntries)):
+  if dictionaryEntries[i][0] == 0:
+    print("Count of 0s: ", dictionaryEntries[i][1], "Percentage of 0s: ", dictionaryEntries[i][1]/len(absWeightValuesConcat))
+  if dictionaryEntries[i][0] == 1:
+    print("Count of 1s: ", dictionaryEntries[i][1], "Percentage of 1s: ", dictionaryEntries[i][1]/len(absWeightValuesConcat))
